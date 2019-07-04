aed9d7dee3912ec0ca6bef484545fe7fcb927580
PICHERIT <etienne.picherit@cgi.com>
2019-07-04 13:40:55 +0200

    ajout jar avec dépendance + scroll sur le champ rue

--- src/main/java/com/cgi/udev2/swing/exo/presentation/DonneesPersonelles.java
+++ src/main/java/com/cgi/udev2/swing/exo/presentation/DonneesPersonelles.java
@@ -6,13 +6,10 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
